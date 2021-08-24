a4973e7cb4c53cad7f882fa5f69c5b59f6ba8cf1
Liam Curtin <liamcurtin86@gmail.com>
2021-08-24 12:08:21 +0100

    data types

---
+++ VariablesAndDatatypes/VariablesAndDatatypes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDatatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favourite number?");
+            //string favouriteNumber = Console.ReadLine();
+            //int favnum = Convert.ToInt32(favouriteNumber);
+            //int Total = favnum + 5;
+            //Console.WriteLine("Your favourite number plus 5 is: " + Total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursworked = 42;
+            sbyte currenttemp = -23;
+            char RandomLetter = 'Y';
+            char questionMark = '\u2103';
+            decimal moneyinbank = 100.5m;
+            double heightinCM = 205.343;
+            float secondsLeft = 2.63f;
+            short tempMars = -341;
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            string StudyStatus = Convert.ToString(isStudying);
+
+            Console.WriteLine(StudyStatus);
+            Console.ReadLine();
+        }
+    }
+}
