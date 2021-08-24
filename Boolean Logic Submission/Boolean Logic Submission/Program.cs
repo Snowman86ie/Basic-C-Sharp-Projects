63062c2bd7879a5cfff2e8550594511fe1307c15
Liam Curtin <liamcurtin86@gmail.com>
2021-08-24 15:55:32 +0100

    Boolean Logic Submission

---
+++ Boolean Logic Submission/Boolean Logic Submission/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Boolean_Logic_Submission
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Car Insurance Approval");
+            Console.WriteLine();
+
+            Console.WriteLine("Please answer the following questions.");
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int PersonAge = Convert.ToInt32(age);
+            Console.WriteLine("Have you ever had a DUI violation? Please answer \"true\" or \"false\".");
+            string temp = Console.ReadLine();
+            bool dui = Convert.ToBoolean(temp);
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speed = Console.ReadLine();
+            int SpeedingTickets = Convert.ToInt32(speed);
+            Console.WriteLine();
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(PersonAge > 15 && dui == false && SpeedingTickets == 0);
+
+
+            Console.ReadLine();
+        }
+    }
+}
