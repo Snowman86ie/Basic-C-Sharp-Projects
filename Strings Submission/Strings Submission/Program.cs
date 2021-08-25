04e6727ef2995f7e7b8d7859942d04ad0e284cbd
Liam Curtin <liamcurtin86@gmail.com>
2021-08-25 15:58:04 +0100

    String Submission

---
+++ Strings Submission/Strings Submission/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+
+namespace Strings_Submission
+{
+    class Program
+    {
+        static void Main()
+        {
+            string firstname = "Liam";
+            string phrase = "Hello world, i'm new to all this!";
+            string lastname = "Jones";
+
+            Console.WriteLine(phrase + " "+ firstname +" "+ lastname);
+
+            firstname = firstname.ToUpper();
+            Console.WriteLine(firstname);
+
+            StringBuilder paragraph = new StringBuilder(phrase);
+            Console.WriteLine(paragraph);
+            paragraph.Append(" ");
+            paragraph.Append(firstname);
+            paragraph.Append(" ");
+            Console.WriteLine(paragraph);
+            paragraph.Append(lastname);
+            Console.WriteLine(paragraph);
+
+            Console.ReadLine();
+
+        }
+    }
+}
