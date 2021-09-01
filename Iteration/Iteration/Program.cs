e688ad3dc24fe0a32b90f22cc20647f8daa5bfb2
Liam Curtin <liamcurtin86@gmail.com>
2021-09-01 17:54:41 +0100

    update

---
+++ Iteration/Iteration/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Console App Assignment");
+        Console.WriteLine();
+
+        List<string> names = new List<string>{ "Jessie", "Dave", "Alan", "Paul" };
+        List<string> updatednames = new List<string>();
+        Console.WriteLine("Please enter a name:");
+        string data = Console.ReadLine();
+        Console.WriteLine();
+       
+        for (int i =0; i<names.Count; i++)
+        {
+            if (names.Count >= i)
+            {
+                names.Add(data);
+            }
+        }
+
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+        Console.ReadLine();
+
+    }
+
+}
+
