e4616c81f44132d6613ee2a61c28ac1a175eeb6e
Liam Curtin <liamcurtin86@gmail.com>
2021-09-02 17:35:38 +0100

    Console App

--- Iteration/Iteration/Program.cs
+++ Iteration/Iteration/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 
 class Program
@@ -9,27 +10,97 @@ class Program
         Console.WriteLine("Console App Assignment");
         Console.WriteLine();
 
-        List<string> names = new List<string>{ "Jessie", "Dave", "Alan", "Paul" };
-        List<string> updatednames = new List<string>();
+        List<string> names = new List<string> { "Jessie", "Dave", "Alan", "Paul" };
         Console.WriteLine("Please enter a name:");
         string data = Console.ReadLine();
         Console.WriteLine();
-       
-        for (int i =0; i<names.Count; i++)
+
+        while (string.IsNullOrEmpty(data))
         {
-            if (names.Count >= i)
-            {
-                names.Add(data);
-            }
+            Console.WriteLine("Please enter a name:");
+            data = Console.ReadLine();
+        }
+        if (!string.IsNullOrEmpty(data))
+        {
+            names.Add(data);
         }
 
-        foreach (string name in names)
+        for (int i = 0; i < names.Count; i++)
         {
-            Console.WriteLine(name);
+            Console.WriteLine(names[i]);
+        }
+
+        Console.WriteLine();
+        List<string> pets = new List<string> { "dog", "cat", "bird", "snake", "horse", "goat" };
+        Console.WriteLine("Please pick a favourite pet:");
+        string pet = Console.ReadLine();
+
+        while (string.IsNullOrEmpty(pet))
+        {
+            Console.WriteLine("Please pick a favourite pet:");
+            pet = Console.ReadLine();
+        }
+        if (pets.Contains(pet))
+        {
+            for (int j = 0; j < pets.Count; j++)
+            {
+                if (pet == pets[j])
+                {
+                    Console.WriteLine("You pet is number " + j + " in the database.");
+                    goto End;
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Your pet isn't in our database.");
+        }
+
+    End:
+
+        List<string> greetings = new List<string> { "hi", "hello", "good day", "afternoon", "good night", "afternoon" };
+        Console.WriteLine();
+        Console.WriteLine("Please pick a favourite greeting:");
+        string greet = Console.ReadLine();
+        while (string.IsNullOrEmpty(greet))
+        {
+            Console.WriteLine("Please pick a favourite greeting:");
+            greet = Console.ReadLine();
+        }
+        if (greetings.Contains(greet))
+        {
+            for (int j = 0; j < greetings.Count; j++)
+            {
+                if (greet == greetings[j])
+                {
+                    Console.WriteLine("Your greeting is number " + j + " in the database.");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Your greeting isn't in our database.");
+        }
+
+        Console.WriteLine();
+        List<string> items = new List<string> { "morning", "afternoon", "evening", "night", "morning" };
+        List<string> duplicate = new List<string>();
+
+
+        foreach (string item in items)
+        {
+            if (!duplicate.Contains(item))
+            {
+                duplicate.Add(item);
+                Console.WriteLine(item + " " + " is not a duplicate");
+            }
+            else
+            {
+                Console.WriteLine(item + " " + "is a duplicate");
+            }
         }
         Console.ReadLine();
 
     }
-
 }
 
