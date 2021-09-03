fc2937d41c983e2c0edf8f60b905c882dfe9eb0e
Liam Curtin <liamcurtin86@gmail.com>
2021-09-03 12:26:09 +0100

    Exception Assignment

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            List<int> numbers = new List<int> { 1, 4, 8, 12, 16, 20 };
+            Console.WriteLine("Please enter a number:");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            foreach (int num in numbers)
+                {
+                    int answer = (num / userNumber);
+                    Console.WriteLine(answer);
+                }
+            
+        }
+        catch(FormatException ex)
+        {
+            Console.WriteLine("Please select a whole number to divide by:");
+        }
+        catch(DivideByZeroException ex)
+        {
+            Console.WriteLine("Please dont divide by zero");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.ReadLine();
+        }
+        Console.WriteLine("Program continues");
+        Console.ReadLine();
+
+
+    }
+}
+
