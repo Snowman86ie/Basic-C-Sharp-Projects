b2816e2167095182bb4cbd15ced5cb68dbab05df
Liam Curtin <liamcurtin86@gmail.com>
2021-09-03 16:18:38 +0100

    Method Assignment

---
+++ MethodAssignment/MethodAssignment/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    public class Math
+    {
+        public static int Double(int number)
+        {
+            int answer = number*2;
+            return answer;
+        }
+        public static int product(int number)
+        {
+            int ans2 = number*number;
+            return ans2;
+
+        }
+        public static int divide(int number)
+        {
+            int ans3 = number/2;
+            return ans3;
+
+        }
+    }
+}
---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Method Assignment");
+            Console.WriteLine();
+            Console.WriteLine("Please enter a number below:");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            int result1 = Math.Double(userNumber);
+            Console.WriteLine("Double your number is: " + result1);
+            Console.WriteLine();
+            int result2 = Math.product(userNumber);
+            Console.WriteLine("Your number mutliplied by itself is: " + result2);
+            Console.WriteLine();
+            int result3 = Math.divide(userNumber);
+            Console.WriteLine("Your number divied by two is: " + result3);
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
