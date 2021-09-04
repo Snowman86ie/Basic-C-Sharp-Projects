23c4b02f7d52e7d77c4b223ecd3ca0f6a5f3ccee
Liam Curtin <liamcurtin86@gmail.com>
2021-09-04 17:38:04 +0100

    Class and Method Assignment 3

---
+++ Class and Method Assignment 3/Class and Method Assignment 3/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_and_Method_Assignment_3
+{
+    class Math
+    {
+        public void Addition(int num1, int num2)
+        {
+            int answer = num1 *2;
+            Console.WriteLine(num1 +" Mutliplied by 2 is: "+ answer);
+            Console.WriteLine("The second number used is: " + num2);
+            return;
+        }
+    }
+}
---
+++ Class and Method Assignment 3/Class and Method Assignment 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_and_Method_Assignment_3
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            
+            int num1 = 3, num2 = 5;
+            math.Addition(num1 , num2);
+            Console.ReadLine();
+        }
+    }
+}
