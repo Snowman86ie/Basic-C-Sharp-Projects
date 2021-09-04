3937ed3664c1f6089608aa0612cc61926fc33cf9
Liam Curtin <liamcurtin86@gmail.com>
2021-09-04 12:15:05 +0100

    Class and method assignment 1

---
+++ ClassandMethods Assignment1/ClassandMethods Assignment1/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassandMethods_Assignment1
+{
+    class Math
+    {
+        public static int Addition(int number)
+        {
+            int answer = number + number;
+            return answer;
+        }
+        public static decimal Addition(decimal number)
+        {
+            decimal answer = number * number;
+            return answer;
+        }
+        public static string Addition(string number)
+        {
+            int temp = Convert.ToInt32(number);
+            string answer2 = (temp -4).ToString();
+            return answer2;
+        }
+    }
+}
---
+++ ClassandMethods Assignment1/ClassandMethods Assignment1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassandMethods_Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Class and Method Assignment");
+            Console.WriteLine();
+            Console.WriteLine("Please enter a number below:");
+            decimal userNumber = Convert.ToDecimal(Console.ReadLine());
+            
+            decimal result = Math.Addition(Convert.ToDecimal(userNumber));
+            Console.WriteLine("Your number mutliplied by itself is: " + result);
+            Console.WriteLine();
+            Console.ReadLine();
+
+            int result1 = Math.Addition(Convert.ToInt32(userNumber));
+            Console.WriteLine("Double your number is: " + result1);
+            Console.WriteLine();
+            Console.ReadLine();
+
+            string result2 = Math.Addition(Convert.ToString(userNumber)) ;
+            int temp = Convert.ToInt32(result2);
+            Console.WriteLine("Your number minus four is: " + result2);
+            Console.WriteLine();
+            Console.ReadLine();
+
+
+        }
+    }
+}
