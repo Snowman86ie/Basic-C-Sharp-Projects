dc0a0a1acb839188979e7de6d3df7b0293ed73aa
Liam Curtin <liamcurtin86@gmail.com>
2021-09-05 13:14:52 +0100

    Class Method Assignment

---
+++ Class Method Assignment/Class Method Assignment/Math.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Assignment
+{
+    class Math
+    {
+        public void Division(int num1)
+        {
+            int result = num1 / 2;
+            Console.WriteLine();
+            Console.WriteLine("Your number divied by 2 is:" + result);
+            
+        }
+
+        public static int Addition(int num1)
+        {
+            int result = num1 + 5;
+            return result;
+        }
+
+        public static int Addition(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+    }
+    static class Numbers
+    {
+
+    }
+}
---
+++ Class Method Assignment/Class Method Assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Class Method Assignment");
+            Console.WriteLine();
+            Console.WriteLine("Please enter a number:");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            math.Division(userNumber);
+            Console.WriteLine();
+            int result = Math.Addition(userNumber);
+            Console.WriteLine("Your number plus five is:" + result);
+            Console.WriteLine();
+            int result2 = Math.Addition(userNumber, 15);
+            Console.WriteLine("Your number plus 15 is:" + result2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
