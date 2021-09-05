ddbc468112688ac9670bbcdc4c53fc25240fd2e0
Liam Curtin <liamcurtin86@gmail.com>
2021-09-05 17:58:01 +0100

    Abstract Classes Assignment

---
+++ Abstract Classes Assignment/Abstract Classes Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee();
+
+            person.FirstName = "Sample";
+            person.LastName = "Student";
+
+            person.SayName();
+
+
+            Console.ReadLine();
+        }
+    }
+}
