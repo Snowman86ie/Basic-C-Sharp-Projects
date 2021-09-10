d1666ed5e158ed39d04bcb197f93004af4c784d4
Liam Curtin <liamcurtin86@gmail.com>
2021-09-10 15:15:54 +0100

    Generic Assignment

---
+++ Generics Assignment/Generics Assignment/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Assignment
+{
+    public class Employee<T>
+    {
+        public List<T> Things { get; set; }
+    
+    
+        public void Print()
+        {
+            foreach(T thing in Things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+    }
+
+}
---
+++ Generics Assignment/Generics Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            employee.Things = new List<string>();
+
+            employee.Things.Add("hello");
+            employee.Things.Add("hi");
+            employee.Things.Add("good day");
+
+            employee.Print();
+
+            Employee<int> employee1 = new Employee<int>();
+            employee1.Things = new List<int>();
+            employee1.Things.Add(1);
+            employee1.Things.Add(2);
+            employee1.Things.Add(3);
+
+            employee1.Print();
+
+            Console.ReadLine();
+        }
+    }
+}
