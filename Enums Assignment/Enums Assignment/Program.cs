69473146d07d9bd7605d0441459f332a493388da
Liam Curtin <liamcurtin86@gmail.com>
2021-09-10 17:36:35 +0100

    Enum Assignment

---
+++ Enums Assignment/Enums Assignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day:");
+            string userDay = Console.ReadLine();
+
+            DaysofWeek day1;
+            
+            
+            try
+            {
+                if (Enum.TryParse<DaysofWeek>(userDay, true, out day1))
+                {
+                    Console.WriteLine("The current day is: "+ day1);
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+
+            Console.ReadLine();
+        }
+
+        public enum DaysofWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
