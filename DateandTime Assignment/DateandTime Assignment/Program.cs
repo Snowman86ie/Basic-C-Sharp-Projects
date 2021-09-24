e54a8472332956588956abab94cc55e61c8930e8
Liam Curtin <liamcurtin86@gmail.com>
2021-09-24 11:13:52 +0100

    DateTime Assignment

---
+++ DateandTime Assignment/DateandTime Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DateandTime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Now;
+            Console.WriteLine("Current Date and time is: {0}" , time);
+            Console.WriteLine("Please enter a number below:");
+            
+            int data = Convert.ToInt32(Console.ReadLine());
+            
+            DateTime newTime = time.AddHours(data);
+            Console.WriteLine("Time in {0} hours is: {1}",data, newTime);
+
+            Console.ReadLine();
+        }
+    }
+}
