03b972bab58d04e1ddd525a51996da87dc734279
Liam Curtin <liamcurtin86@gmail.com>
2021-09-25 11:19:53 +0100

    update

--- Additional Features Assignment/Additional Features Assignment/Program.cs
+++ Additional Features Assignment/Additional Features Assignment/Program.cs
@@ -6,15 +6,28 @@ using System.Threading.Tasks;
 
 namespace Additional_Features_Assignment
 {
-    class Program
+    public class Program
     {
+        public Program() : this(10)
+        {
+            int age = 10;
+            Console.WriteLine(age);
+            Console.ReadLine();
+        }
+        public Program(int age)
+        {
+            age = 5;
+            Console.WriteLine(age);
+            Console.ReadLine();
+
+        }
         static void Main(string[] args)
         {
+            Program program = new Program();
             const string name = "Additional Features";
             var data = 21;
 
             Console.WriteLine("We have {0} new {1}", data, name);
-
 
             Console.ReadLine();
         }
