e78b97a47ca2792069c62767889554d9dcaeb4e6
Liam Curtin <liamcurtin86@gmail.com>
2021-09-25 11:03:31 +0100

    Exception Handling Assignment

---
+++ Exception Handling Assignment/Exception Handling Assignment/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter your age:");
+                int age = Convert.ToInt32(Console.ReadLine());
+                DateTime year = DateTime.Now;
+                if (age <= 0)
+                {
+                    throw new NotSupportedException();
+                }
+                
+                int yearBorn = (year.Year - age);
+
+                Console.WriteLine("You were born in the year {0}", yearBorn);
+                Console.ReadLine();
+            }
+            catch (NotSupportedException)
+            {
+                Console.WriteLine("Please enter a positive number for age.");
+                Console.ReadLine();
+                return;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("A string is not accepted.");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error has occured, please contact your system admin!");
+                Console.ReadLine();
+                return;
+            }
+            
+        }
+    }
+}
